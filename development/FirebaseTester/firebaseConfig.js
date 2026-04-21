f2ee60f53a7cc2d19b3c5018795d830e4ebfce15
Simone Graziussi <simone.graziussi@gmail.com>
2026-04-21 21:54:29 +0200

    Move dev resources to root

---
+++ development/FirebaseTester/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCyEftc-HkQsYPenIz9iZ1i-qDq3zf01Qc",
+    authDomain: "media-tracker-dev-db499.firebaseapp.com",
+    databaseURL: "https://media-tracker-dev-db499.firebaseio.com",
+    projectId: "media-tracker-dev-db499",
+    storageBucket: "media-tracker-dev-db499.firebasestorage.app",
+    messagingSenderId: "59496103241",
+    appId: "1:59496103241:web:6e07d1539cf6e4ee312e98"
+};
+
